cf8702f45f71219fa71273a9e83bfd22b47d03b2
pratikpatil01 <pratik.devyanitechnologies@gmail.com>
2023-10-11 22:17:28 +0530

    add manufacturer api implement

--- src/Network_call/ApiEndPoints.js
+++ src/Network_call/ApiEndPoints.js
@@ -12,7 +12,7 @@ const ApiEndPoints = {
   Resetpassword: `${APPConfig.API_URL}/resetpassword`,
   DataList: `${APPConfig.API_URL}/list`,
   ManufacturerListForAdmin: `${APPConfig.API_URL}/get-manufacturer-list`,
-  ManufacturerAddForAdmin: `${APPConfig.API_URL}/add-manufacturer`,
+  ManufacturerAdd: `${APPConfig.API_URL}/add-manufacturer`,
   UserListForAdmin: `${APPConfig.API_URL}/get-user-list`,
   FileUpload: `${APPConfig.API_URL}/upload`,
   GetTypeList: `${APPConfig.API_URL}/get-type-list`,
--- src/store/reducers/manufacturer.tsx
+++ src/store/reducers/manufacturer.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import ApiEndPoints from 'src/Network_call/ApiEndPoints';
 import ApiServices from 'src/Network_call/apiservices';
 import { dispatch } from 'src/store';
@@ -7,81 +7,100 @@ const initialState = {
   count: 0,
   ManufacturerData: [],
   manufacturerDetails: [],
-  GetContentTypeList:[],
+  GetContentTypeList: [],
 
   isLoading: true
 };
+
+export const addManufactur = createAsyncThunk(
+  'addManufactur',
+  async (payload: any) => {
+    console.log('respont', payload);
+    const response = await ApiServices(
+      'post',
+      ApiEndPoints.ManufacturerAdd,
+      payload
+    );
+    return response;
+  }
+);
+export const editManufactur = createAsyncThunk(
+  'editManufactur',
+  async (payload: any) => {
+    const response = await ApiServices(
+      'post',
+      ApiEndPoints.ManufacturerAdd,
+      payload
+    );
+    return response;
+  }
+);
+export const GetManufacturerList = createAsyncThunk(
+  'GetManufacturerList',
+  async (payload: any) => {
+    const response = await ApiServices(
+      'post',
+      ApiEndPoints.ManufacturerAdd,
+      payload
+    );
+    return response;
+  }
+);
+
 const manufacturerSlice = createSlice({
   name: 'manufacturerSlice',
   initialState,
 
-  reducers: {
-    increse: (state) => {
-      state.count = state.count + 1;
-    },
-    decrese: (state) => {
-      state.count = state.count - 1;
-    },
-
-    addManufacturer: (state, action) => {
-      console.log('add', action.payload, 'state', state);
-    },
-    manufacturerList: (state, action) => {
-      state.ManufacturerData = action.payload;
-      console.log('action.payload', action.payload);
-    },
-    manufacturerDetails: (state, action) => {
-      state.manufacturerDetails = action.payload;
-      console.log('action.payload', action.payload);
-    },
-    GetContentTypeList: (state, action) => {
-      state.GetContentTypeList = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder: any) => {
+    builder
+      .addCase(addManufactur.pending, (state: any) => {
+        state.isLoading = true;
+      })
+      .addCase(addManufactur.fulfilled, (state: any, action: any) => {
+        state.isLoading = false;
+      })
+      .addCase(editManufactur.pending, (state: any) => {
+        state.isLoading = true;
+      })
+      .addCase(editManufactur.fulfilled, (state: any, action: any) => {
+        state.isLoading = false;
+      });
   }
 });
 
-export const {
-  addManufacturer,
-  manufacturerList,
-  manufacturerDetails,
-  increse,
-  decrese
-} = manufacturerSlice.actions;
-
 export default manufacturerSlice.reducer;
 
-const GetManufacturerList = async (payload) => {
-  const response = await ApiServices('post', payload, ApiEndPoints.DataList);
-  dispatch(manufacturerSlice.actions.manufacturerList(response));
-};
-const getManufacturerDetails = async (payload) => {
-  const response = await ApiServices('get', payload, ApiEndPoints);
-  dispatch(manufacturerSlice.actions.addManufacturer(response));
-};
-const addManufactur = async (payload) => {
-  const response = await ApiServices('get', payload, ApiEndPoints);
-  dispatch(manufacturerSlice.actions.addManufacturer(response));
-};
-const decreseCount = async () => {
-  dispatch(manufacturerSlice.actions.decrese());
-};
+// const GetManufacturerList = async (payload: any) => {
+//   const response = await ApiServices('post', payload, ApiEndPoints.DataList);
+//   dispatch(manufacturerSlice.actions.manufacturerList(response));
+// };
+// const getManufacturerDetails = async (payload) => {
+//   const response = await ApiServices('get', payload, ApiEndPoints);
+//   dispatch(manufacturerSlice.actions.addManufacturer(response));
+// };
+// // const addManufactur = async (payload: any) => {
+// //   const response = await ApiServices('get', payload, ApiEndPoints);
+// //   dispatch(manufacturerSlice.actions.addManufacturer(response));
+// // };
+// const decreseCount = async () => {
+//   dispatch(manufacturerSlice.actions.decrese());
+// };
 
-const increseCount = async () => {
-  dispatch(manufacturerSlice.actions.increse());
-};
+// const increseCount = async () => {
+//   dispatch(manufacturerSlice.actions.increse());
+// };
 
-const GetContentTypeList = async (payload) => {
-  const response = await ApiServices('get', ApiEndPoints.GetContentTypeList);
-  dispatch(manufacturerSlice.actions.GetContentTypeList(response));
-};
+// const GetContentTypeList = async (payload: any) => {
+//   const response = await ApiServices('get', ApiEndPoints.GetContentTypeList);
+//   dispatch(manufacturerSlice.actions.GetContentTypeList(response));
+// };
 
-
-export {
-  GetManufacturerList,
-  getManufacturerDetails,
-  addManufactur,
-  increseCount,
-  decreseCount,
-  GetContentTypeList
-};
-
+// export {
+//   GetManufacturerList,
+//   getManufacturerDetails,
+//   addManufactur,
+//   increseCount,
+//   decreseCount,
+//   GetContentTypeList
+// };
